7c6b38a8df56953d02fad89981d752f5c2b8fa55
esksami <eskelinenn.sami@gmail.com>
2017-11-07 14:13:08 +0200

    productlist and refactoring

---
+++ imports/plugins/custom/olga-core/server/addCustomGroups.js
@@ -0,0 +1,32 @@
+import { Roles } from "meteor/alanning:roles";
+import { Shops, Groups }  from "/lib/collections";
+import { Reaction } from "/lib/api";
+
+
+export const defaultSupplierRoles = [ "guest", "account/profile", "product", "tag", "index", "cart/checkout", "cart/completed"];
+
+export function createCustomGroups() {
+  const shopId = Reaction.getPrimaryShopId();
+  console.log('PRIMARY SHOPID: ' + shopId)
+
+  const allGroups = Groups.find({}).fetch();
+
+  const roles = {
+    "supplier": defaultSupplierRoles,
+  };
+
+  Object.keys(roles).forEach(groupKeys => {
+    const groupExists = allGroups.find(grp => grp.slug === groupKeys && grp.shopId === shopId);
+
+    if (groupExists) {
+      Groups.remove({'slug': groupKeys, 'shopId': shopId});
+    }
+
+    Groups.insert({
+      name: groupKeys,
+      slug: groupKeys,
+      permissions: roles[groupKeys],
+      shopId: shopId
+    });
+  });
+};
