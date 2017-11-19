c3e20aafe1f77cd3669cdb36eab1038fd90171c8
Juha <juha.kangas@welho.com>
2017-11-20 00:00:21 +0200

    Modaali toimii, testit päivittämättä

--- imports/plugins/custom/olga/client/components/products/supplierProductsListReact.js
+++ imports/plugins/custom/olga/client/components/products/supplierProductsListReact.js
@@ -12,15 +12,21 @@ class SupplierProductsListReact extends Component {
     super(props);
 
     this.state = {
-      modalIsOpen: false,
+      contractModalIsOpen: false,
+      deliveryModalIsOpen: false,
       productId: "id",
-      openQuantity: 2,
+      openQuantity: 0,
+      contractedQuantity: 0,
       productName: "name",
-      supplyQuantity: 0
+      supplyQuantity: 0,
+      deliveryQuantity: 0
     };
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
+    this.openContractModal = this.openContractModal.bind(this);
+    this.closeContractModal = this.closeContractModal.bind(this);
+    this.openDeliveryModal = this.openDeliveryModal.bind(this);
+    this.closeDeliveryModal = this.closeDeliveryModal.bind(this);
     this.updateSupplyQuantity = this.updateSupplyQuantity.bind(this);
+    this.updateDeliveryQuantity = this.updateDeliveryQuantity.bind(this);
   }
 
     alertOptions = {
@@ -38,35 +44,71 @@ class SupplierProductsListReact extends Component {
       });
     }
 
-    openModal(productId, productTitle, openQuantity) {
+    openContractModal(productId, productTitle, openQuantity) {
       this.setState({
-        modalIsOpen: true,
+        contractModalIsOpen: true,
+        deliveryModalIsOpen: false,
         productId: productId,
         productName: productTitle,
         openQuantity: openQuantity,
-        supplyQuantity: 0
+        contractedQuantity: 0,
+        supplyQuantity: 0,
+        deliveryQuantity: 0
       });
     }
 
-    closeModal() {
+    openDeliveryModal(productId, productTitle, contractedQuantity) {
+      this.setState({
+        contractModalIsOpen: false,
+        deliveryModalIsOpen: true,
+        productId: productId,
+        productName: productTitle,
+        openQuantity: 0,
+        contractedQuantity: contractedQuantity,
+        supplyQuantity: 0,
+        deliveryQuantity: 0
+      });
+    }
+
+    closeContractModal() {
       if (this.state.supplyQuantity == 0 || this.state.openQuantity <= 0) {
-        this.setState({ modalIsOpen: false });
+        this.setState({ contractModalIsOpen: false });
       } else {
-        this.setState({ modalIsOpen: false });
+        this.setState({ contractModalIsOpen: false });
         const contractId = Meteor.call("supplyContracts/create", this.state.productId, parseInt(this.state.supplyQuantity));
         this.showAlert("Toimitussopimus tehty (" + this.state.productName + " " + this.state.supplyQuantity + " kpl)", "success");
       }
     }
 
+    closeDeliveryModal() {
+      if(this.state.deliveryQuantity == 0 || this.state.contractedQuantity <= 0) {
+        this.setState({ deliveryModalIsOpen: false });
+        console.log("Ei toimitusilmoitusta");
+      } else {
+        this.setState({ deliveryModalIsOpen: false });
+        //const deliveryId = Meteor.call("supplyContracts/fulfill", this.state.productId, parseInt(this.state.deliveryQuantity));
+        console.log("Toimitetaan " + this.state.productName + ", " + this.state.deliveryQuantity + " kpl");
+        this.showAlert("Toimitusilmoitus tehty (" + this.state.productName + " " + this.state.deliveryQuantity + " kpl", "success");
+      }
+    }
+
     updateSupplyQuantity(e) {
       if (Number.isNaN(e.target.value) || !Number.isInteger(Number(e.target.value))) {
-        console.log("Invalid input");
         this.showAlert("Toimitusmäärän on oltava kokonaisluku", "error");
       } else if (Number(e.target.value) > this.state.openQuantity) {
-        console.log("Too large quantity");
         this.showAlert("Voit toimittaa enintään avoinna olevan määrän", "error");
       } else {
         this.setState({ supplyQuantity: e.target.value });
+      }
+    }
+
+    updateDeliveryQuantity(e) {
+      if (Number.isNaN(e.target.value) || !Number.isInteger(Number(e.target.value))) {
+        this.showAlert("Toimitusmäärän on oltava kokonaisluku", "error");
+      } else if (Number(e.target.value) > this.state.contractedQuantity) {
+        this.showAlert("Voit toimittaa enintään sovitun määrän", "error");
+      } else {
+        this.setState({ deliveryQuantity: e.target.value });
       }
     }
 
@@ -83,7 +125,8 @@ class SupplierProductsListReact extends Component {
             <SupplierProductsListItem
               productStat={cellInfo.original}
               userStatus={this.props.userStatus}
-              showContractModal={this.openModal}
+              showContractModal={this.openContractModal}
+              showDeliveryModal={this.openDeliveryModal}
             />
           )
         },
@@ -97,7 +140,8 @@ class SupplierProductsListReact extends Component {
             <SupplierProductsListBtns
               productStat={cellInfo.original}
               userStatus={this.props.userStatus}
-              showContractModal={this.openModal}
+              showContractModal={this.openContractModal}
+              showDeliveryModal={this.openDeliveryModal}
             />
           )
         }
@@ -106,8 +150,8 @@ class SupplierProductsListReact extends Component {
       return (
         <div>
               <Modal
-                isOpen={this.state.modalIsOpen}
-                onRequestClose={this.state.closeModal}
+                isOpen={this.state.contractModalIsOpen}
+                onRequestClose={this.state.closeContractModal}
                 contentLabel="Create supplyContract"
                 className={{
                     base: "contractModal",
@@ -126,10 +170,38 @@ class SupplierProductsListReact extends Component {
                 <input type="number" id="quantity" name="quantity" className="right-justified" min="0" max={this.state.openQuantity}
                     onChange={this.updateSupplyQuantity} value={this.state.supplyQuantity}/></h3>
                 <div>
-                    <button id="cancelModal" className="rui btn btn-primary flat olga-listing-btn-default pull-right"
-                        onClick={() => this.closeModal()} >Peruuta</button>
+                    <button id="cancelContractModal" className="rui btn btn-primary flat olga-listing-btn-default pull-right"
+                        onClick={() => this.closeContractModal()} >Peruuta</button>
                     <button id="confirmContract" className="rui btn btn-primary flat olga-listing-btn-success pull-right"
-                        onClick={() => this.closeModal($('#quantity').val())} >Vahvista</button>
+                        onClick={() => this.closeContractModal($('#quantity').val())} >Vahvista</button>
+                </div>
+              </Modal>
+
+              <Modal
+                isOpen={this.state.deliveryModalIsOpen}
+                onRequestClose={this.state.closeDeliveryModal}
+                contentLabel="Create delivery"
+                className={{
+                    base: "deliveryModal",
+                    afterOpen: "deliveryModal_after-open",
+                    beforeClose: "deliveryModal_before-close"
+                }}
+                overlayClassName={{
+                    base: "deliveryModalOverlay",
+                    afterOpen: "deliveryModalOverlay_after-open",
+                    beforeClose: "deliveryModalOverlay_before-close"
+                }}
+                >
+                <h2 id="deliveryModalTitle">{this.state.productName}</h2>
+                <h3>Sovittu toimitusmäärä: <span id="contractedQuantity">{this.state.contractedQuantity}</span> </h3>
+                <h3><label htmlFor="deliveryQuantity">Toimitettava määrä: </label>
+                <input type="number" id="deliveryQuantity" name="deliveryQuantity" className="right-justified" min="0" max={this.state.contractedQuantity}
+                    onChange={this.updateDeliveryQuantity} value={this.state.deliveryQuantity}/></h3>
+                <div>
+                    <button id="cancelDeliveryModal" className="rui btn btn-primary flat olga-listing-btn-default pull-right"
+                        onClick={() => this.closeDeliveryModal()} >Peruuta</button>
+                    <button id="confirmDelivery" className="rui btn btn-primary flat olga-listing-btn-success pull-right"
+                        onClick={() => this.closeDeliveryModal($('#quantity').val())} >Vahvista</button>
                 </div>
               </Modal>
 
