43355223e49cca06007598cd2a22db2b59fb38d5
Juha <juha.kangas@welho.com>
2017-11-21 16:36:24 +0200

    First take toimitustietojen varastoinnista

---
+++ imports/plugins/custom/olga/lib/collections/schemas/deliveries.js
@@ -0,0 +1,29 @@
+import { SimpleSchema } from "meteor/aldeed:simple-schema";
+
+export const Delivery = new SimpleSchema({
+    userId: {
+        type: String        
+    },
+    productId: {
+        type: String
+    },
+    deliveryQuantity: {
+        type: Number,
+        defaultValue: 0
+    },
+    supplyContracts: {
+        type: [String]
+    },
+    createdAt: {
+        type: Date,
+        autoValue: function() {
+            if (this.isInsert) {
+                return new Date();
+            } else if (this.isUpsert) {
+                return {$setOnInsert: new Date()};
+            } else {
+                this.unset();  // Prevent user from supplying their own value
+            }
+        }
+    }
+})
