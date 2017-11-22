80c3c21619ebe059d8bbd511259bd435339e4c5d
Tatu Helander <tatu.helander@helsinki.fi>
2017-11-22 14:28:51 +0200

    First steps

--- imports/plugins/custom/accounts/client/index.js
+++ imports/plugins/custom/accounts/client/index.js
@@ -1,2 +1,7 @@
 export { default as MainDropdown } from "./components/mainDropdown";
-export { default as MainDropdownContainer } from "../../accounts/client/containers/mainDropdown";
+export { default as GroupsTable } from "./components/groupsTable";
+export { default as GroupsTableCell } from "./components/groupsTableCell";
+export { default as GroupsTableManageItemsButton } from "./components/groupsTableManageItemsButton";
+
+export { default as MainDropdownContainer } from "./containers/mainDropdown";
+export { default as accountsDashboardContainer} from "./containers/accountsDashboardContainer";
