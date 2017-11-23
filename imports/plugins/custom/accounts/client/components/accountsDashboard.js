913cfa9e439969c0aadcca81d333aa9725731d53
Erika Ylenius <eylenius@dhcp-asv-116.helsinki.fi>
2017-11-23 13:58:48 +0200

    modaalin tyyliä muutettu

--- imports/plugins/custom/accounts/client/components/accountsDashboard.js
+++ imports/plugins/custom/accounts/client/components/accountsDashboard.js
@@ -119,6 +119,30 @@ class AccountsDashboard extends Component {
           isOpen={this.state.itemModalIsOpen}
           onRequestClose={this.state.closeItemModal}
           contentLabel={"Osoita tuotteita toimittajalle"}
+          style={{
+            overlay : {
+            position          : 'fixed',
+            top               : 0,
+            left              : 0,
+            right             : 0,
+            bottom            : 0,
+            backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+          },
+          content : {
+            position                   : 'absolute',
+            top                        : '40px',
+            left                       : '40px',
+            right                      : '40px',
+            bottom                     : '40px',
+            border                     : '1px solid #ccc',
+            background                 : '#fff',
+            overflow                   : 'auto',
+            WebkitOverflowScrolling    : 'touch',
+            borderRadius               : '4px',
+            outline                    : 'none',
+            padding                    : '20px'
+          }
+        }}
           className={{
             base: "itemModal",
             afterOpen: "itemModal_after-open",
