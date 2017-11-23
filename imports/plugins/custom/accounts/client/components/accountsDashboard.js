8f4f84ea7f89bdfd00c71a70200d5d7a7b24cf33
Tatu Helander <tatu.helander@helsinki.fi>
2017-11-23 15:47:31 +0200

    Modaalin tyylittely siirretty omaan tiedostoon

--- imports/plugins/custom/accounts/client/components/accountsDashboard.js
+++ imports/plugins/custom/accounts/client/components/accountsDashboard.js
@@ -119,32 +119,6 @@ class AccountsDashboard extends Component {
           isOpen={this.state.itemModalIsOpen}
           onRequestClose={this.state.closeItemModal}
           contentLabel={"Osoita tuotteita toimittajalle"}
-          style={{
-            overlay : {
-              zIndex            : 9999,
-              position          : 'absolute',
-              top               : 0,
-              left              : 0,
-              right             : 0,
-              bottom            : 0,
-              backgroundColor   : 'rgba(255, 255, 255, 0.75)',
-            },
-            content : {
-              zIndex                     : 9999,
-              position                   : 'absolute',
-              top                        : '40px',
-              left                       : '40px',
-              right                      : '40px',
-              bottom                     : '40px',
-              border                     : '1px solid #ccc',
-              background                 : '#fff',
-              overflow                   : 'auto',
-              WebkitOverflowScrolling    : 'touch',
-              borderRadius               : '4px',
-              outline                    : 'none',
-              padding                    : '20px'
-            }
-          }}
           className={{
             base: "itemModal",
             afterOpen: "itemModal_after-open",
