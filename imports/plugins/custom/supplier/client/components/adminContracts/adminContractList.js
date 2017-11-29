a7b24f408a0b4826411c37ff5aeadba68ebd852f
esksami <eskelinenn.sami@gmail.com>
2017-11-29 16:57:41 +0200

    refactor adminContracts

--- imports/plugins/custom/supplier/client/components/adminContracts/adminContractList.js
+++ imports/plugins/custom/supplier/client/components/adminContracts/adminContractList.js
@@ -5,11 +5,13 @@ import PropTypes from "prop-types";
 import { registerComponent, composeWithTracker } from "@reactioncommerce/reaction-components";
 
 import ReactTable from "react-table";
+import { VelocityComponent, VelocityTransitionGroup } from 'velocity-react';
+import { SortableTablePagination } from "/imports/plugins/core/ui/client/components/table/sortableTableComponents";
+import Avatar from "react-avatar";
 import './styles.less';
 
 import { ContractTotals, ProductTotals } from 'imports/plugins/custom/olga-core/lib/collections/collections';
-import ReactTransitionGroup from 'react-addons-css-transition-group'; // ES6
-import { VelocityComponent, VelocityTransitionGroup } from 'velocity-react';
+
 
 class AdminContractList extends Component {
   constructor(props) {
@@ -21,7 +23,6 @@ class AdminContractList extends Component {
   }
 
   render() {   
-    const util = require('util'); 
     const productTotalsCol = [
       {
         Header: "Tuote",
@@ -33,6 +34,7 @@ class AdminContractList extends Component {
         Cell: info => (
           <span className="product-name"> {info.original.product.title} </span>
         ),
+        className: "contract-table-name-header"
       },
       {
         Header: "Tuotannossa",
@@ -41,7 +43,8 @@ class AdminContractList extends Component {
           <div className="contract-total"> {info.original.production} </div>
         ),
         Filter: () => <span></span>,
-        maxWidth: 190
+        maxWidth: 120,
+        className: "contract-table-mid-header" 
       },
       {
         Header: "Lähetetty",
@@ -50,7 +53,8 @@ class AdminContractList extends Component {
           <div className="contract-total"> {info.original.delivery} </div>
         ),
         Filter: () => <span></span>,
-        maxWidth: 190
+        maxWidth: 120,
+        className: "contract-table-mid-header"      
       },
       {
         Header: "Vastaanotettu",
@@ -59,7 +63,8 @@ class AdminContractList extends Component {
           <div className="contract-total"> {info.original.received} </div>
         ),
         Filter: () => <span></span>,
-        maxWidth: 190
+        maxWidth: 120,
+        className: "contract-table-mid-header" 
       },
       {
         Header: "Tuottajia",
@@ -68,56 +73,64 @@ class AdminContractList extends Component {
           <div className="contract-total"> {info.original.users.length} </div>
         ),
         Filter: () => <span></span>,
-        maxWidth: 190
+        maxWidth: 120,
+        className: "contract-table-mid-header"
       }
     ];
 
     const supplierTotalsCol = [
       {
         Header: "",
-        id: "empty",
-        Cell: info => (
-          <div className="empty"> </div>
-        ),
-        Filter: () => <span></span>,
-        maxWidth: 35
-      },
-      {
-        Header: "",
         filterMethod: (filter, row) => (
           row[filter.id].toLowerCase().includes(filter.value.toLowerCase())
         ),
         id: "supplier",
-        Cell: info => (
-          <span className="product-name"> {info.original.user.name} </span>
+        Cell: row => (
+          <div style={{ display: "inline-flex" }}>
+            <Avatar
+              email={row.original.user.email}
+              round={true}
+              name={row.original.user.name}
+              size={25}
+              className="rui-order-avatar"
+            />
+            <div className="supplier-full-name">
+              <strong >
+                {row.original.user.name}
+              </strong>
+            </div>
+          </div>
         ),
       },
       {
         Header: "",
         id: "production",
         Cell: info => (
-          <div className="contract-total"> {info.original.production} </div>
+          <div className="contract-total  -left"> {info.original.production} </div>
         ),
         Filter: () => <span></span>,
-        maxWidth: 190
+        maxWidth: 120,
+        headerClassName: 'contract-total-header -left'
       },
       {
         Header: "",
         id: "delivery",
         Cell: info => (
-          <div className="contract-total"> {info.original.delivery} </div>
+          <div className="contract-total -mid"> {info.original.delivery} </div>
         ),
         Filter: () => <span></span>,
-        maxWidth: 190
+        maxWidth: 120,
+        headerClassName: 'contract-total-header -mid'
       },
       {
         Header: "",
         id: "received",
         Cell: info => (
-          <div className="contract-total"> {info.original.received} </div>
+          <div className="contract-total -right"> {info.original.received} </div>
         ),
         Filter: () => <span></span>,
-        maxWidth: 190
+        maxWidth: 120,
+        headerClassName: 'contract-total-header -right'
       },
       {
         Header: "",
@@ -126,12 +139,43 @@ class AdminContractList extends Component {
           <div className="empty"> </div>
         ),
         Filter: () => <span></span>,
-        maxWidth: 190
+        maxWidth: 120
       }
     ];
 
+    let getTheadProps = () => {
+      return {
+        className: "contract-table-thead"
+      };
+    };
+
+    let getTheadThProps = () => {
+      return {
+        className: "contract-table-thead"
+      };
+    };
+
+    let getTrGroupProps = () => {
+      return {
+        className: "contract-table-tr-group"
+      };
+    };
+
+    let getTableProps = () => {
+      return {
+        className: "contract-table-list"
+      };
+    };
+
+    let getTrProps = () => {
+      return {
+        className: "contract-table-detail-tr"
+      };
+    };
+
+
     return (
-      <div >
+      <div className="contract-table-container">
         {_.isEmpty(this.props.productTotals) ?
         <div className='empty-view-message'> 
           No contracts found
@@ -141,11 +185,23 @@ class AdminContractList extends Component {
           data={this.props.productTotals}
           columns={productTotalsCol}
           defaultPageSize={10}
-          className="OlgaTable -highlight -striped"
+          className="rui order table -highlight table-header-visible"
           minRows={1}
           expanded={this.state.expanded}
           onExpandedChange={expanded => this.setState({ expanded })}
           filterable
+          headerClassName="contract-table-mid-header"
+          getTableProps={getTableProps}
+          getTheadProps={getTheadProps}
+          getTrGroupProps={getTrGroupProps}
+          getTheadThProps={getTheadThProps}
+          getPaginationProps={() => {
+            return {
+              className: "order-table-pagination-visible"
+            };
+          }}
+          PaginationComponent={SortableTablePagination}
+
           SubComponent={row => {
             const contractTotals = ContractTotals.find({
               productId: row.original.productId
@@ -158,13 +214,15 @@ class AdminContractList extends Component {
                 runOnMount={true}
               >
                 {this.state.expanded[row.index] !== false ? 
+                <div className="supplier-table-container">
                   <ReactTable
                     data={contractTotals}
                     columns={supplierTotalsCol}
-                    className="OlgaTable SubTable -highlight -striped"
+                    className="rui order table supplier-table -highlight "
                     showPagination={false}
                     minRows={1}
                   />
+                </div>
                 : undefined
                 }
               </VelocityTransitionGroup>
