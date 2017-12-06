24ce75aab060ed752d1e17210ef4d00aa268d8ed
esksami <eskelinenn.sami@gmail.com>
2017-12-06 14:22:42 +0200

    ContractItem definition

--- imports/plugins/custom/olga-core/lib/collections/collections.js
+++ imports/plugins/custom/olga-core/lib/collections/collections.js
@@ -11,7 +11,14 @@ import *  as Schemas from "./schemas";
  */
 Accounts.attachSchema(Schemas.productlistSchema)
 
-//Meteor.users.schema = Accounts;
+
+/**
+ * ContractItems Collection
+ * @ignore
+ */
+export const ContractItems = new Mongo.Collection("ContractItems");
+
+ContractItems.attachSchema(Schemas.ContractItemSchema);
 
 
 /**
@@ -20,17 +27,23 @@ Accounts.attachSchema(Schemas.productlistSchema)
  */
 export const ContractTotals = new Mongo.Collection("ContractTotals", {
   transform: (totals) => {
-    totals.product = Products.findOne({'_id': totals.productId});
+    //totals.product = Products.findOne({'_id': totals.productId});
     totals.user = Accounts.findOne({'_id': totals.userId});
+    //totals.info = ContractItems.findOnde({'productId': totals.productId})
 
     return totals;
   }
 });
 
-export const ProductTotals = new Mongo.Collection("ProductTotals", {
-  transform: (totals) => {
-    totals.product = Products.findOne({_id: totals.productId});
+export const SimpleContractTotals = new Mongo.Collection("SimpleContractTotals");
 
-    return totals;
-  }
-});
+export const VariantContractTotals = new Mongo.Collection("VariantContractTotals");
+
+
+/**
+ * ContractItem Aggregates per simple product and variant-option
+ * @ignore
+ */
+export const OpenSimpleTotals = new Mongo.Collection("OpenSimpleTotals");
+
+export const OpenVariantOptionTotals = new Mongo.Collection("OpenVariantOptionTotals");
